f863060f84e04815cdafdaa38bdc472d6e25e76b
zhongjixiuxing <anxing131@gmail.com>
2022-05-01 06:13:38 +0000

    feat: add replace.py sample script

---
+++ README-replace.md
@@ -0,0 +1,5 @@
+# Python opencv 车牌识别 & 替换
+
+### 参考资料
+https://blog.csdn.net/zaibeijixing/article/details/105018331
+https://learnopencv.com/homography-examples-using-opencv-python-c/
---
+++ replace.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = 'jinmu333'
+
+import threading
+import time
+import tkinter as tk
+import cv2
+import lib.img_function as predict
+import lib.img_math as img_math
+import lib.img_excel as img_excel
+import lib.img_sql as img_sql
+from lib.img_api import api_pic
+import lib.screencut as screencut
+from threading import Thread
+from tkinter import ttk
+from tkinter.filedialog import *
+from PIL import Image, ImageTk, ImageGrab
+import tkinter.messagebox
+import requests
+from time import sleep
+from hyperlpr import *
+import numpy as np
+
+## 4个点的坐标排序，从左下角顺时针开始
+def order_points(pts):
+    ''' sort rectangle points by clockwise '''
+    
+    sort_x = pts[np.argsort(pts[:, 0]), :]
+    
+    Left = sort_x[:2, :]
+    Right = sort_x[2:, :]
+    # Left sort
+    Left = Left[np.argsort(Left[:,1])[::-1], :]
+    # Right sort
+    Right = Right[np.argsort(Right[:,1]), :]
+    
+    return np.concatenate((Left, Right), axis=0)
+
+def im_cover(im_dst_path, dst_points, im_cover_path, isReduce, ppath):
+    img_icon = cv2.imread(im_cover_path)
+    im_dst = cv2.imread(im_dst_path)
+    # nrows, cols = img_icon.shape
+    nrows, ncols = img_icon.shape[:2]
+
+    # 左上角开始，顺时针
+    src_points = np.float32([
+        [0, 0],
+        [ncols, 0],
+        [ncols, nrows],
+        [0, nrows],
+    ])
+    dst_points = np.float32(dst_points)
+
+    # 排序坐标
+    orderPoints = order_points(dst_points)
+    dst_points = np.float32([
+        orderPoints[1],
+        orderPoints[2],
+        orderPoints[3],
+        orderPoints[0],
+    ])
+
+    im_temp = im_dst.copy()
+    # cv2.imshow("Image", im_temp)
+    h = cv2.getPerspectiveTransform(src_points, dst_points)
+    im_out = cv2.warpPerspective(img_icon, h, (im_temp.shape[1],im_temp.shape[0]), dst=im_temp)
+
+    # cv2.imshow("Warped Source Image", im_out)
+    pts_dst = np.int32(dst_points)
+
+    cv2.fillConvexPoly(im_dst, pts_dst, (255, 255, 255))
+    im_dst = im_dst + im_temp
+    # cv2.imshow("DST", im_dst)
+    cv2.imwrite('./tmp/result/' + ppath, im_dst)
+
+def resize(w, h, pil_image):
+    w_box = 200
+    h_box = 50
+    f1 = 1.0*w_box/w
+    f2 = 1.0*h_box/h
+    factor = min([f1, f2])
+    width = int(w*factor)
+    height = int(h*factor)
+    return pil_image.resize((width, height), Image.ANTIALIAS)
+
+def run(ppath): 
+    pic_path = './car_pic/'+ppath
+    picImg = Image.open(pic_path)
+
+    predictor = predict.CardPredictor()
+    predictor.train_svm()
+
+    apistr = None
+    img_bgr = img_math.img_read(pic_path)
+    first_img, oldimg = predictor.img_first_pre(img_bgr)
+    
+    first_img_instance = Image.fromarray(first_img)
+    oldimg_instance = Image.fromarray(first_img)
+    first_img_instance.save('/app/tmp/ax_test_first_img.png')
+    oldimg_instance.save('/app/tmp/ax_test_oldimg.png')
+
+    r_c, roi_c, color_c, box_point = predictor.img_color_contours(first_img, oldimg, True)
+    box_point1 = box_point
+    r_color, roi_color, color_color, box_point =  predictor.img_only_color(oldimg, oldimg, first_img, True)
+
+    try:
+        Plate = HyperLPR_PlateRecogntion(img_bgr)
+        r_c = Plate[0][0]
+        r_color = Plate[0][0]
+    except:
+        pass
+    if not color_color:
+        color_color = color_c
+    if not color_c:
+        color_c = color_color
+
+    # 没有识别到车牌
+    if (len(str(r_color)) == 0): 
+        print('没有识别到车牌: ', ppath)
+        return
+    # if (roi_color is not None):
+        # roi = cv2.cvtColor(roi_color, cv2.COLOR_BGR2RGB)
+        # roi = Image.fromarray(roi)
+        # w, h = roi.size
+        # roi.save('/app/tmp/ax_test_roi.png')
+        # roi_resized = resize(w, h, roi)
+        # roi.save('/app/tmp/ax_test_roi_resize.png')
+        # pil_image_resized = self.resize(w, h, roi)
+
+    # 车牌区域存在问题
+    if (box_point is None):
+        # 尝试用 box_point1
+        box_point = box_point1
+    if (box_point is None):
+        return 
+
+    isReduce = False # 是否比例变了
+    if (picImg.size[0] >= 1000):
+        isReduce = True
+        rate = 1000 / picImg.size[0]
+        box_point[0][0] = box_point[0][0] / rate
+        box_point[0][1] = box_point[0][1] / rate
+        box_point[1][0] = box_point[1][0] / rate
+        box_point[1][1] = box_point[1][1] / rate
+        box_point[2][0] = box_point[2][0] / rate
+        box_point[2][1] = box_point[2][1] / rate
+        box_point[3][0] = box_point[3][0] / rate
+        box_point[3][1] = box_point[3][1] / rate
+    
+    im_cover(pic_path, box_point, '/app/car_pic/logo.png', isReduce, ppath=ppath)
+    # cv2.waitKey(0)
+
+if __name__ == '__main__':
+    pic_paths = [
+        'IMG_1758.jpeg',
+        'car4.jpg',
+        'car5.jpg',
+        'car7.jpg',
+        'ganzou4.png',
+        'ganzou5.png',
+        'ganzou6.png',
+        'ganzou7.png',
+        'ganzou08.png',
+        'ganzou353.png',
+        'IMG_1759.jpeg',
+        'IMG_1761.jpeg',
+        'IMG_1762.jpeg',
+        'IMG_1764.jpeg',
+        'IMG_1769.jpeg',
+        'IMG_1774.jpeg',
+        'IMG_1870.jpeg',
+        'IMG_1872.jpeg',
+        'IMG_1873.jpeg',
+        'IMG_1875.jpeg',
+        'IMG_1877.jpeg',
+        'timg.jpg',
+        'timg1.jpg',
+        'timg3.jpg',
+        'timg2.jpg',
+        'timg4.jpg',
+        'wA87271.jpg',
+        'wATH859.jpg',
+        'wAUB816.jpg',
+    ]
+    for pic_path in pic_paths:
+        run(pic_path)
